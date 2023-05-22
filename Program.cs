5a1f9f04e34ea76a2c038b14155e5824129ecea8
Fajri-Febriansyah <febriansyahfajri7@gmail.com>
2023-05-22 14:59:39 +0700

    intial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrograman_class_dan_object_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi taxi  = new taxi();
+
+            taxi.DriverName = " QOSIM QODIR BIN AHMAD AL HAMZAH ";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 3;
+
+            taxi.taxiinfo();
+            taxi.PickUpPassenger();
+            taxi.PickDownPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemrograman_class_dan_object_4
+{
+    internal class taxi
+    {
+    public string DriverName { get; set; }
+    public bool OnDuty { get; set; }
+    public int NumPassenger { get; set; }
+ 
+    public void taxiinfo()
+    {
+    string status;
+        Console.WriteLine( " DRIVER NAME : {0}", DriverName);
+
+    if (OnDuty)
+        {
+            status = "YES";
+        }else{
+            status = "NO";
+        }
+            Console.WriteLine(" CALL OF DUTY : {0}", status);
+            Console.WriteLine(" NUMBER OF PASENGGER  : {0} ", NumPassenger);
+        }
+    public void PickUpPassenger()
+        {
+            Console.WriteLine("{0} THE DRIVER IS PICKING UP PASSENGERS ", DriverName);
+        }
+        public void PickDownPassenger()
+        {
+            Console.WriteLine("{0} THE DRIVER IS DROPPING OFF PASSENGERS ", DriverName);
+        }
+        
+    }
+}
